7eb33cb4f184f1270e86099cd93d66a994e28597
ackottsi <kottsickandrewc@johndeere.com>
2021-04-09 09:54:32 -0500

    used bootstrap cards to reformat AllPost Layout.  changed background color

--- src/component/AllPost.js
+++ src/component/AllPost.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../AllPost.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card, Button} from 'react-bootstrap'
 
 
 const AllPost = (props) =>{
@@ -10,21 +12,35 @@ const AllPost = (props) =>{
         
         props.hpData.map(post=>(
           <div key={post.id} className='post-container'>
-           
-            <h2>{post.title}</h2>
-            <p>{post.description}</p>
-            <Link to={`/PostPage/${post.id}`}>
-            <img src={post.image_url} alt='' className='blog-pic'/>
-            </Link>
-          
+           <Card style={{ width: '18rem' }}>
+                <Link to={`/PostPage/${post.id}`}>
+                    <Card.Img variant="top" src={post.image_url} />
+                </Link>
+                <Card.Body>
+                    <Card.Title>{post.title}</Card.Title>
+                    <Card.Text>
+                    {post.description}
+                    </Card.Text>
+                    <Button variant="primary" onClick={()=>props.deletePost(post)}>
+                        Delete Post
+                    </Button>
+                </Card.Body>
+            </Card>
 
-            <button 
+            {/* <h2>{post.title}</h2>
+            <p>{post.description}</p>
+         
+            <img src={post.image_url} alt='' className='blog-pic'/>
+           
+           */}
+
+            {/* <button 
                 className='delete-button' 
                 id={post.id}
                 onClick={()=>props.deletePost(post)}
             >
             Delete Post
-            </button>
+            </button> */}
           </div>
         ))
 
