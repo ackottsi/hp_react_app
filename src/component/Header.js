5d0cdc31d6df81e096a3db2b9d76eddba783f6a1
ackottsi <kottsickandrewc@johndeere.com>
2021-04-11 21:21:07 -0500

    refactored code to use Link for HomePage navigation in Header

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../Header.css'
 import {Container, Button, Row, Col} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 
 const Header=(props)=>{
     return(
         <Row>
-            <Col><Button variant="primary">HomePage</Button>{' '}</Col>
+            <Col><Link to='/' className='btn btn-primary' variant="primary">HomePage</Link>{' '}</Col>
             <Col> <Button variant="primary">New Post</Button>{' '}</Col>
             <Col> <Button variant="primary">Something</Button>{' '}</Col>
             
